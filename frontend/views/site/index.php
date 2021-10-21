0054197921314b513035a2b5bf2a8fbb236f6055
Myklon <livroklon@gmail.com>
2021-10-21 21:08:37 +0300

    feat: first commit

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -18,12 +18,9 @@ $this->title = 'My Yii Application';
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Заголовок1</h2>
+                <h2>Привет мир</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>С этого места мы начали.</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
